8c8feeccbe764738de94e35c593783a759a30a85
Facugarciar0805 <facugarciar@gmail.com>
2026-03-22 17:33:10 -0300

    feat: pagina auth con request al backend existosa
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

---
+++ client/src/routes/auth/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    let name="";
+    let email='';
+    let password='';
+
+    async function register(){
+        const respuesta = await fetch('http://localhost:3000/auth/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name, email, password })
+        });
+
+        if (respuesta.ok) {
+            alert("¡Usuario creado!");
+        }
+
+    }
+
+</script>
+
+<input bind:value={name} placeholder="Nombre" />
+<input bind:value={email} placeholder="Email" />
+<input bind:value={password} placeholder="Password" />
+<button onclick={() => register()}>Registrarse</button>
--- server/src/router.rs
+++ server/src/router.rs
@@ -2,10 +2,23 @@ use axum::Router;
 use crate::data::state::{SharedState};
 use crate::routes::user::user_routes;
 use crate::routes::auth::auth_routes;
+use tower_http::cors::{Any, CorsLayer};
+use axum::http::Method;
+
+
 
 
 pub fn create_router(state: SharedState) -> Router {
+
+    let cors = CorsLayer::new()
+        // Permitimos que el front en el puerto 5173 nos hable
+        .allow_origin("http://localhost:5173".parse::<axum::http::HeaderValue>().unwrap())
+        // Permitimos los métodos que necesites (POST para registro, etc)
+        .allow_methods([Method::POST, Method::GET])
+        // Muy importante: permitir el encabezado Content-Type para el JSON
+        .allow_headers([axum::http::header::CONTENT_TYPE]);
     Router::new()
         .merge(user_routes(state.clone()))
         .nest("/auth", auth_routes(state.clone()))
+        .layer(cors)//este layer tiene que ir al final de la creacion del Router por si dsp hay que agregar otros nest
 }
