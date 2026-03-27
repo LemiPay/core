6578b0fe058eec51c01b21adf5a810ab3c2263d4
Facundo García Rosselli <162722335+Facugarciar0805@users.noreply.github.com>
2026-03-27 20:41:06 -0300

    Tabla Groups con model (#36)
    * feat: tabla de groupos (sql, schema and model)
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * fix shell script
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * [PROBLEMITA] no esta andando el enum para el model
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * [PROBLEMITA] no esta andando el enum para el model
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * fix: enum status in group
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * fix: enum status in group
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * added git add to script
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    ---------
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- server/src/schema.rs
+++ server/src/schema.rs
@@ -1,5 +1,25 @@
 // @generated automatically by Diesel CLI.
 
+pub mod sql_types {
+    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "group_status"))]
+    pub struct GroupStatus;
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::GroupStatus;
+
+    group (id) {
+        id -> Uuid,
+        name -> Text,
+        description -> Text,
+        status -> GroupStatus,
+        created_at -> Date,
+        updated_at -> Date,
+    }
+}
+
 diesel::table! {
     user (id) {
         id -> Uuid,
@@ -8,3 +28,5 @@ diesel::table! {
         name -> Text,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(group, user,);
