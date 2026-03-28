d2266add223bfc7824c3a5bafcb5895f81e814dd
MateoDi9z <dzmateozd@protonmail.com>
2026-03-28 17:06:50 -0300

    feat: models

--- server/src/models/proposal.rs
+++ server/src/models/proposal.rs
@@ -1,1 +1,34 @@
+use crate::schema::proposal;
+use chrono::NaiveDateTime;
+use diesel::{Insertable, Queryable, Selectable};
+use diesel_derive_enum::DbEnum;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
+#[derive(Debug, DbEnum, Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
+#[db_enum(existing_type_path = "crate::schema::sql_types::ProposalStatus")]
+pub enum ProposalStatus {
+    Pending,
+    Approved,
+    Rejected,
+    Executed,
+}
+
+#[derive(Queryable, Serialize, Selectable)]
+#[diesel(table_name = proposal)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Proposal {
+    pub id: Uuid,
+    pub group_id: Uuid,
+    pub created_by: Uuid,
+    pub status: ProposalStatus,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name = proposal)]
+pub struct NewProposal {
+    pub group_id: Uuid,
+    pub created_by: Uuid,
+}
--- server/src/models/proposals/new_member.rs
+++ server/src/models/proposals/new_member.rs
@@ -1,1 +1,12 @@
+use crate::schema::new_member_proposal;
+use diesel::{Insertable, Queryable, Selectable};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
+#[derive(Queryable, Insertable, Serialize, Deserialize, Selectable)]
+#[diesel(table_name = new_member_proposal)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewMemberProposal {
+    pub proposal_id: Uuid,
+    pub new_member_id: Uuid,
+}
