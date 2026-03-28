d23bdf44174c080a3df3a790394c34f19a908b7b
Facundo García Rosselli <162722335+Facugarciar0805@users.noreply.github.com>
2026-03-28 08:37:26 -0300

    Feature: tabla user in group (#38)
    * feat: useringroup migrations
    
    * [BUG] solo commiteo pq tuve un bug medio raro que no me dejaba correr las migrations, las corri manual y anduvo
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * fix bug: migrations run and schema created
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * feat: insertable and queryable model for user in group
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * added comments
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * fix: Modify down.sql to drop tables and types with CASCADE
    
    ---------
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    Co-authored-by: MateoDi9z <dzmateozd@protonmail.com>
    Co-authored-by: Mateo J. Diaz <60207256+MateoDi9z@users.noreply.github.com>

--- server/migrations/2026-03-27-135818-0000_group/down.sql
+++ server/migrations/2026-03-27-135818-0000_group/down.sql
@@ -1,3 +1,3 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE "group";
+DROP TABLE "group" CASCADE;
 DROP TYPE "group_status";
--- server/src/schema.rs
+++ server/src/schema.rs
@@ -2,6 +2,14 @@
 
 pub mod sql_types {
     #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "group_member_status"))]
+    pub struct GroupMemberStatus;
+
+    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "group_role"))]
+    pub struct GroupRole;
+
+    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
     #[diesel(postgres_type(name = "group_status"))]
     pub struct GroupStatus;
 }
@@ -29,4 +37,22 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(group, user,);
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::GroupRole;
+    use super::sql_types::GroupMemberStatus;
+
+    user_in_group (user_id, group_id) {
+        user_id -> Uuid,
+        group_id -> Uuid,
+        role -> GroupRole,
+        status -> GroupMemberStatus,
+        joined_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::joinable!(user_in_group -> group (group_id));
+diesel::joinable!(user_in_group -> user (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(group, user, user_in_group,);
