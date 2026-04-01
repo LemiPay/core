da2e826530d12beeea466f4a591c9c70f592d84b
Facugarciar0805 <facugarciar@gmail.com>
2026-04-01 14:48:38 -0300

    delete unused dependencies
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- server/src/helpers/validations.rs
+++ server/src/helpers/validations.rs
@@ -1,7 +1,4 @@
 use crate::errors::app_error::AppError;
-use crate::repositories::traits::group_repo::GroupRepository;
-use std::sync::Arc;
-use uuid::Uuid;
 
 pub fn require_non_empty(value: Option<String>, field: &str) -> Result<String, AppError> {
     let value = value.ok_or(AppError::BadRequest(format!("{field} empty")))?;
