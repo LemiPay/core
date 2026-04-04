4edf0ed7fb273f66f249c230511151d237492d86
MateoDi9z <dzmateozd@protonmail.com>
2026-04-04 12:08:08 -0300

    fix: copi fix

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -14,9 +14,9 @@
 	let error = $state('');
 
 	async function login_user() {
-	    error = ''
+		error = '';
 		status = true;
-		
+
 		const response = await api.login(data);
 
 		if (!isSuccess(response)) {
@@ -27,7 +27,7 @@
 
 		authStore.login(response.body.token);
 		status = null;
-		
+
 		data = {
 			email: '',
 			password: ''
@@ -40,7 +40,7 @@
 </script>
 
 <AuthLayout title="Log in to your account" description="Enter your details to access the platform.">
-	<form onsubmit={login_user} onchange={() => status = false} class="flex flex-col space-y-6">
+	<form onsubmit={login_user} onchange={() => (status = false)} class="flex flex-col space-y-6">
 		<!-- Success Message -->
 		{#if status === null && !error}
 			<div
