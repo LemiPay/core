0e7931edcca067a8e9fe4c55fd1c10a42c2dc89e
Facugarciar0805 <facugarciar@gmail.com>
2026-04-09 20:17:31 -0300

    fix migrations
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- server/migrations/2026-04-09-220430-0000_currency/down.sql
+++ server/migrations/2026-04-09-220430-0000_currency/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE currency;
+DROP TABLE IF EXISTS currency;
--- server/migrations/2026-04-09-220430-0000_currency/up.sql
+++ server/migrations/2026-04-09-220430-0000_currency/up.sql
@@ -1,12 +1,12 @@
 -- Your SQL goes here
 CREATE TABLE currency(
-    currency_id UUID PRIMARY KEY,
+    currency_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
     ticker TEXT UNIQUE NOT NULL
 );
 INSERT INTO currency (currency_id, name, ticker)
 VALUES (
-           gen_random_uuid(),
+           '33de6c7c-62a2-4182-813a-9005183be70d',
            'USD Coin',
            'USDC'
        );
