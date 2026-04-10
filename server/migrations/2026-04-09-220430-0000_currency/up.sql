9c52404ea69a02dcad03ca3de54114854a6273f7
MateoDi9z <dzmateozd@protonmail.com>
2026-04-10 10:33:52 -0300

    fix: nit fixes

--- server/migrations/2026-04-09-220430-0000_currency/up.sql
+++ server/migrations/2026-04-09-220430-0000_currency/up.sql
@@ -1,12 +1,15 @@
 -- Your SQL goes here
-CREATE TABLE currency(
+CREATE TABLE IF NOT EXISTS currency(
     currency_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
     ticker TEXT UNIQUE NOT NULL
 );
+
+-- Insert initial currency
 INSERT INTO currency (currency_id, name, ticker)
 VALUES (
            '33de6c7c-62a2-4182-813a-9005183be70d',
            'USD Coin',
            'USDC'
-       );
+       )
+ON CONFLICT (ticker) DO NOTHING;
