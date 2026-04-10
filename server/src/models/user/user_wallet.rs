254a657fb522a58b625e194c81e09a8554c5cad6
MateoDi9z <dzmateozd@protonmail.com>
2026-04-10 10:03:40 -0300

    fix: bad imports & unused code

--- server/src/models/user/user_wallet.rs
+++ server/src/models/user/user_wallet.rs
@@ -8,12 +8,15 @@ use crate::schema::user_wallet;
 #[derive(Queryable, Selectable, Serialize)]
 #[diesel(table_name = user_wallet)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
+#[allow(dead_code)] // TODO: remove after implemented
 pub struct UserWallet {
     pub id: Uuid,
     pub address: String,
     pub user_id: Uuid,
     pub balance: BigDecimal,
     pub currency_id: Uuid,
+    pub created_at: chrono::NaiveDateTime,
+    pub updated_at: chrono::NaiveDateTime,
 }
 
 #[derive(Insertable, Deserialize)]
