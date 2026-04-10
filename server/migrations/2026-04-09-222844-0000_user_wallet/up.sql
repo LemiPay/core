f06acdfbc450a2f2dad0169243f666697b97ddc4
MateoDi9z <dzmateozd@protonmail.com>
2026-04-10 10:17:58 -0300

    fix: pk constraints

--- server/migrations/2026-04-09-222844-0000_user_wallet/up.sql
+++ server/migrations/2026-04-09-222844-0000_user_wallet/up.sql
@@ -13,5 +13,16 @@ CREATE TABLE IF NOT EXISTS user_wallet(
 
     -- reglas de negocio:
     UNIQUE(user_id, currency_id), -- un usuario no puede tener dos wallets de la misma moneda
-    UNIQUE(address, currency_id)  -- una dirección no puede tener dos wallets de la misma moneda
+    UNIQUE(address, currency_id),  -- una dirección no puede tener dos wallets de la misma moneda
+    
+    -- foreign keys
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES "user"(id)
+        ON DELETE RESTRICT,
+
+    CONSTRAINT fk_currency
+        FOREIGN KEY (currency_id)
+        REFERENCES "currency"(id)
+        ON DELETE RESTRICT
 );
