497c9ec160e26924a44b7bd7ff446f55b0b85fe5
MateoDi9z <dzmateozd@protonmail.com>
2026-04-11 20:52:40 -0300

    chore: tx & participant migration

---
+++ server/migrations/2026-04-11-233559-0000_transaction/up.sql
@@ -0,0 +1,84 @@
+-- Your SQL goes here
+-- =========================
+-- ENUM
+-- =========================
+CREATE TYPE transaction_type AS ENUM (
+    'deposit',
+    'withdraw',
+    'expense',
+    'investment'
+);
+
+-- El amount esta dupeado asi validamos consistencia en backend.
+
+-- =========================
+-- TABLE: transaction
+-- =========================
+CREATE TABLE IF NOT EXISTS transaction (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    tx_hash TEXT,
+    amount NUMERIC NOT NULL CHECK (amount > 0),
+
+    user_id UUID NOT NULL,  -- Author
+    group_id UUID NOT NULL,
+    currency_id UUID NOT NULL,
+
+    description TEXT,
+
+    type transaction_type NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT fk_transaction_user
+        FOREIGN KEY (user_id)
+        REFERENCES "user"(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_transaction_group
+        FOREIGN KEY (group_id)
+        REFERENCES "group"(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_transaction_currency
+        FOREIGN KEY (currency_id)
+        REFERENCES "currency"(currency_id)
+        ON DELETE RESTRICT
+);
+
+-- =========================
+-- TABLE: transaction_participant
+-- =========================
+CREATE TABLE IF NOT EXISTS transaction_participant (
+    transaction_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+
+    amount NUMERIC NOT NULL CHECK (amount > 0),
+
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    PRIMARY KEY (transaction_id, user_id),
+
+    CONSTRAINT fk_tp_transaction
+        FOREIGN KEY (transaction_id)
+        REFERENCES transaction(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_tp_user
+        FOREIGN KEY (user_id)
+        REFERENCES "user"(id)
+        ON DELETE CASCADE
+);
+
+-- =========================
+-- INDEXES (A futuro pueden estar buenos)
+-- =========================
+-- CREATE INDEX idx_transaction_user_id ON transaction(user_id);
+-- CREATE INDEX idx_transaction_group_id ON transaction(group_id);
+-- CREATE INDEX idx_transaction_currency_id ON transaction(currency_id);
+-- CREATE INDEX idx_transaction_created_at ON transaction(created_at);
+
+-- CREATE INDEX idx_tp_user_id ON transaction_participant(user_id);
+-- CREATE INDEX idx_tp_transaction_id ON transaction_participant(transaction_id);
--- server/src/schema.rs
+++ server/src/schema.rs
@@ -18,6 +18,10 @@ pub mod sql_types {
     pub struct ProposalStatus;
 
     #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "transaction_type"))]
+    pub struct TransactionType;
+
+    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
     #[diesel(postgres_type(name = "vote_type"))]
     pub struct VoteType;
 }
@@ -86,6 +90,35 @@ diesel::table! {
 }
 
 diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::TransactionType;
+
+    transaction (id) {
+        id -> Uuid,
+        tx_hash -> Nullable<Text>,
+        amount -> Numeric,
+        user_id -> Uuid,
+        group_id -> Uuid,
+        currency_id -> Uuid,
+        description -> Nullable<Text>,
+        #[sql_name = "type"]
+        type_ -> TransactionType,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    transaction_participant (transaction_id, user_id) {
+        transaction_id -> Uuid,
+        user_id -> Uuid,
+        amount -> Numeric,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::table! {
     user (id) {
         id -> Uuid,
         email -> Text,
@@ -141,6 +174,11 @@ diesel::joinable!(new_member_proposal -> proposal (proposal_id));
 diesel::joinable!(new_member_proposal -> user (new_member_id));
 diesel::joinable!(proposal -> group (group_id));
 diesel::joinable!(proposal -> user (created_by));
+diesel::joinable!(transaction -> currency (currency_id));
+diesel::joinable!(transaction -> group (group_id));
+diesel::joinable!(transaction -> user (user_id));
+diesel::joinable!(transaction_participant -> transaction (transaction_id));
+diesel::joinable!(transaction_participant -> user (user_id));
 diesel::joinable!(user_in_group -> group (group_id));
 diesel::joinable!(user_in_group -> user (user_id));
 diesel::joinable!(user_wallet -> currency (currency_id));
@@ -155,6 +193,8 @@ diesel::allow_tables_to_appear_in_same_query!(
     group_wallet,
     new_member_proposal,
     proposal,
+    transaction,
+    transaction_participant,
     user,
     user_in_group,
     user_wallet,
