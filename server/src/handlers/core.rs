1f309f236b73b8f8a265489f84000c4e55bd3246
Facugarciar0805 <facugarciar@gmail.com>
2026-04-20 23:31:55 -0300

    armo route
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

---
+++ server/src/handlers/core.rs
@@ -0,0 +1,33 @@
+use crate::data::state::SharedState;
+use crate::errors::app_error::AppError;
+use crate::security::auth_extractor::AuthUser;
+
+use axum::{
+    Json,
+    extract::{Path, Query, State},
+};
+
+use bigdecimal::BigDecimal;
+use serde::Serialize;
+use uuid::Uuid;
+
+#[derive(Serialize)]
+pub struct UserBalance {
+    pub user_id: Uuid,
+    pub balance: BigDecimal,
+}
+
+#[derive(Serialize)]
+pub struct Balances {
+    pub group_balance: BigDecimal,
+    pub balances: Vec<UserBalance>,
+}
+
+pub async fn get_balances(
+    State(state): State<SharedState>,
+    Path(group_id): Path<Uuid>,
+    user: AuthUser,
+) -> Result<Json<Vec<Balances>>, AppError> {
+    let result;
+    Ok(Json(result))
+}
---
+++ server/src/routes/core.rs
@@ -0,0 +1,18 @@
+use axum::routing::{get, post};
+use axum::{Router, middleware};
+
+use crate::data::state::SharedState;
+use crate::handlers::core::get_balances;
+use crate::security::middlewares::auth::auth_middleware;
+use crate::security::middlewares::is_in_group::is_in_group_middleware;
+
+pub fn core_routes(state: SharedState) -> Router {
+    Router::new()
+        .route("/balances", get(get_balances))
+        .route_layer(middleware::from_fn_with_state(
+            state.clone(),
+            is_in_group_middleware,
+        ))
+        .route_layer(middleware::from_fn(auth_middleware))
+        .with_state(state)
+}
