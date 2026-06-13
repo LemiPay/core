7e8d10d925c39f797319eaf280c02cf9d64a262c
Facugarciar0805 <facugarciar@gmail.com>
2026-06-13 10:39:29 -0300

    agrego el usecase al servicio
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- server/src/application/settlements/pay_settlement/mod.rs
+++ server/src/application/settlements/pay_settlement/mod.rs
@@ -1,3 +1,6 @@
 pub mod dto;
 pub mod error;
 pub mod use_case;
+
+pub use error::PaySettlementError;
+pub use use_case::PaySettlementUseCase;
--- server/src/application/settlements/service.rs
+++ server/src/application/settlements/service.rs
@@ -1,5 +1,7 @@
 use crate::application::settlements::get_settlements::GetSettlementsUseCase;
+use crate::application::settlements::pay_settlement::PaySettlementUseCase;
 
 pub struct SettlementsService {
     pub get_settlements: GetSettlementsUseCase,
+    pub pay_settlement: PaySettlementUseCase,
 }
