94c48c84cb13202f3260014e30b0b64d3d49a20f
MateoDi9z <dzmateozd@protonmail.com>
2026-07-20 12:44:09 -0300

    logs in main

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,15 +6,20 @@ use server::setup::app_builder::build_app;
 
 #[tokio::main]
 async fn main() {
+    println!("=== Checkpoint 1 ===");
     let app = build_app();
 
+    println!("=== Checkpoint 2 ===");
     // 🚀 Server
     use std::net::SocketAddr;
     let addr = env::get_address();
 
+    println!("=== Checkpoint 3 ===");
     println!("Server running on http://{}", addr);
 
     let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("=== Checkpoint 3 ===");
 
     serve(
         listener,
