369202a6128e119afe8f6a1f36222fb6e19e48da
Facugarciar0805 <facugarciar@gmail.com>
2026-07-21 09:29:38 -0300

    cambio favicon
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 
 	import { authStore } from '$lib/stores/auth';
 	import { onMount } from 'svelte';
